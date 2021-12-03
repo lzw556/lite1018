28a7ca3a7a70e082af80756b1625ed873cd5db0e
onlylin <747325123@qq.com>
2021-12-03 09:37:02 +0800

    add measurement type

--- server/domain/po/measurement.go
+++ server/domain/po/measurement.go
@@ -2,9 +2,21 @@ package po
 
 import "gorm.io/gorm"
 
+type MeasurementType uint
+
+const (
+	BoltLooseningMeasurementType MeasurementType = iota + 1
+	BoltElongationMeasurementType
+	CorrosionThicknessMeasurementType
+	PressureMeasurementType
+	FlangeLooseningMeasurementType
+	FlangeElongationMeasurementType
+)
+
 type Measurement struct {
 	gorm.Model
 	Name string `gorm:"type:varchar(64);"`
+	Type uint
 }
 
 func (Measurement) TableName() string {
