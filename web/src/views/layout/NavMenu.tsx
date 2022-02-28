25f5d9281dd91dc3d93feab716030c2da814e945
liuzewei <liuzewei@outlook.com>
2022-02-28 15:36:01 +0800

    feature:optimization for Menu

---
+++ web/src/views/layout/NavMenu.tsx
@@ -0,0 +1,65 @@
+import { Menu } from 'antd';
+import * as React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { Menu as MenuItem } from '../../types/menu';
+import { GetParamValue } from '../../utils/path';
+
+export const NavMenu: React.FC<{ menus: MenuItem[] }> = ({ menus }) => {
+  const { pathname, search } = useLocation();
+  const locale = GetParamValue(search, 'locale');
+  const { SubMenu } = Menu;
+  const renderMenuItem = (children: []) => {
+    return children.map((item: any) => {
+      if (!item.hidden) {
+        if (item.children && item.children.filter((item: any) => !item.hidden).length) {
+          return (
+            <SubMenu key={item.name} title={item.title}>
+              {renderMenuItem(item.children)}
+            </SubMenu>
+          );
+        }
+        return (
+          <Menu.Item key={item.name}>
+            <NavLink to={`${item.path}?locale=${item.name}`}>{item.title}</NavLink>
+          </Menu.Item>
+        );
+      }
+    });
+  };
+
+  if (menus.length === 0) return null;
+  return (
+    <Menu
+      mode='inline'
+      className='ts-menu'
+      defaultSelectedKeys={['devices']}
+      selectedKeys={locale ? locale.split('/') : []}
+      defaultOpenKeys={[pathname.replace('/', '')]}
+    >
+      {menus &&
+        menus.map((item: any) => {
+          if (!item.hidden) {
+            if (item.children && item.children.filter((item: any) => !item.hidden).length) {
+              return (
+                <SubMenu
+                  key={item.name}
+                  title={item.title}
+                  icon={item.icon && <span className={`iconfont ${item.icon}`} />}
+                >
+                  {renderMenuItem(item.children)}
+                </SubMenu>
+              );
+            }
+            return (
+              <Menu.Item
+                key={item.name}
+                icon={item.icon && <span className={`iconfont ${item.icon}`} />}
+              >
+                <NavLink to={`${item.path}?locale=${item.name}`}>{item.title}</NavLink>
+              </Menu.Item>
+            );
+          }
+        })}
+    </Menu>
+  );
+};
