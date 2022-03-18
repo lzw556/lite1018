fd4c9be2daad164d4e4d9e32e92936c1b5baaa6b
liuzewei <liuzewei@outlook.com>
2022-03-18 16:40:18 +0800

    feature:mobile adaptation 13

---
+++ web/src/views/device/detail/information/SingleDeviceDetail.tsx
@@ -0,0 +1,172 @@
+import { Col, Row, Space, Typography } from 'antd';
+import moment from 'moment';
+import * as React from 'react';
+import { Device } from '../../../../types/device';
+import { DeviceType } from '../../../../types/device_type';
+import { isMobile } from '../../../../utils/deviceDetection';
+import { SingleDeviceStatus } from '../../SingleDeviceStatus';
+import DeviceUpgradeSpin from '../../spin/deviceUpgradeSpin';
+const { Text } = Typography;
+
+export const SingleDeviceDetail: React.FC<{ device: Device; upgradeStatus: any }> = ({
+  device,
+  upgradeStatus
+}) => {
+  return (
+    <Row justify={'start'}>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            设备名称
+          </Col>
+          <Col span={isMobile ? 24 : 16}>
+            <Space>
+              {device.name}
+              {upgradeStatus && <DeviceUpgradeSpin status={upgradeStatus} />}
+            </Space>
+          </Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            类型
+          </Col>
+          <Col span={isMobile ? 24 : 16}>{DeviceType.toString(device.typeId)}</Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            MAC地址
+          </Col>
+          <Col span={isMobile ? 24 : 16}>
+            <Text copyable={{ text: device.macAddress, tooltips: ['复制', '复制成功'] }}>
+              {device.macAddress.toUpperCase().macSeparator()}
+            </Text>
+          </Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            型号
+          </Col>
+          <Col span={isMobile ? 24 : 16}>
+            {device.information.model ? device.information.model : '-'}
+          </Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            状态
+          </Col>
+          <Col span={isMobile ? 24 : 16}>
+            <SingleDeviceStatus alertStates={device.alertStates} state={device.state} />
+          </Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            所属网络
+          </Col>
+          <Col span={isMobile ? 24 : 16}>{device.network ? device.network.name : '无'}</Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          {' '}
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            电池电压(mV)
+          </Col>
+          <Col span={isMobile ? 24 : 16}>
+            {device.state && device.typeId !== DeviceType.Gateway
+              ? device.state.batteryVoltage
+              : '-'}
+          </Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            信号强度(dB)
+          </Col>
+          <Col span={isMobile ? 24 : 16}>{device.state ? device.state.signalLevel : '-'}</Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            固件版本号
+          </Col>
+          <Col span={isMobile ? 24 : 16}>
+            {device.information.firmware_version ? device.information.firmware_version : '-'}
+          </Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            最近连接时间
+          </Col>
+          <Col span={isMobile ? 24 : 16}>
+            {device.state.connectedAt
+              ? moment(device.state.connectedAt * 1000).format('YYYY-MM-DD HH:mm:ss')
+              : '-'}
+          </Col>
+        </Row>
+      </Col>
+      <Col span={isMobile ? 12 : 9}>
+        <Row>
+          <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+            硬件版本
+          </Col>
+          <Col span={isMobile ? 24 : 16}>
+            {device.information.product_id ? device.information.product_id : '-'}
+          </Col>
+        </Row>
+      </Col>
+      {device.typeId !== DeviceType.Gateway && device.typeId !== DeviceType.Router && (
+        <Col span={isMobile ? 12 : 9}>
+          <Row>
+            <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+              最近一次采集时间
+            </Col>
+            <Col span={isMobile ? 24 : 16}>
+              {device.data && device.data.timestamp && device.data.timestamp > 0
+                ? moment.unix(device.data.timestamp).local().format('YYYY-MM-DD HH:mm:ss')
+                : '-'}
+            </Col>
+          </Row>
+        </Col>
+      )}
+      {device.typeId === DeviceType.Gateway && (
+        <Col span={isMobile ? 12 : 9}>
+          <Row>
+            <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
+              IP地址
+            </Col>
+            <Col span={isMobile ? 24 : 16}>
+              {device.information.ip_address ? (
+                <Space>
+                  {device.information.ip_address}{' '}
+                  <a
+                    href={`http://${device.information.ip_address}`}
+                    target={'_blank'}
+                    style={{ fontSize: '10pt' }}
+                  >
+                    访问管理界面
+                  </a>
+                </Space>
+              ) : (
+                '-'
+              )}
+            </Col>
+          </Row>
+        </Col>
+      )}
+    </Row>
+  );
+};
--- web/src/views/device/detail/information/index.tsx
+++ web/src/views/device/detail/information/index.tsx
@@ -1,23 +1,16 @@
-import { Col, Row, Skeleton, Space, Tag, Typography } from 'antd';
+import { Skeleton } from 'antd';
 import { FC, useEffect, useState } from 'react';
 import { Device } from '../../../../types/device';
 import '../../index.css';
-import moment from 'moment';
 import ShadowCard from '../../../../components/shadowCard';
-import { ColorHealth, ColorWarn } from '../../../../constants/color';
 import '../../../../string-extension';
 import useSocket, { SocketTopic } from '../../../../socket';
-import DeviceUpgradeSpin from '../../spin/deviceUpgradeSpin';
-import { DeviceType } from '../../../../types/device_type';
-import { SingleDeviceStatus } from '../../SingleDeviceStatus';
-import { isMobile } from '../../../../utils/deviceDetection';
+import { SingleDeviceDetail } from './SingleDeviceDetail';
 
 export interface GatewayInformationProps {
   device: Device;
   isLoading: boolean;
 }
-
-const { Text } = Typography;
 
 const InformationCard: FC<GatewayInformationProps> = ({ device, isLoading }) => {
   const [upgradeStatus, setUpgradeStatus] = useState<any>(device.upgradeStatus);
@@ -37,161 +30,7 @@ const InformationCard: FC<GatewayInformationProps> = ({ device, isLoading }) => 
   return (
     <ShadowCard>
       <Skeleton loading={isLoading}>
-        <Row justify={'start'}>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                设备名称
-              </Col>
-              <Col span={isMobile ? 24 : 16}>
-                <Space>
-                  {device.name}
-                  {upgradeStatus && <DeviceUpgradeSpin status={upgradeStatus} />}
-                </Space>
-              </Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                类型
-              </Col>
-              <Col span={isMobile ? 24 : 16}>{DeviceType.toString(device.typeId)}</Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                MAC地址
-              </Col>
-              <Col span={isMobile ? 24 : 16}>
-                <Text copyable={{ text: device.macAddress, tooltips: ['复制', '复制成功'] }}>
-                  {device.macAddress.toUpperCase().macSeparator()}
-                </Text>
-              </Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                型号
-              </Col>
-              <Col span={isMobile ? 24 : 16}>
-                {device.information.model ? device.information.model : '-'}
-              </Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                状态
-              </Col>
-              <Col span={isMobile ? 24 : 16}>
-                <SingleDeviceStatus alertStates={device.alertStates} state={device.state} />
-              </Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                所属网络
-              </Col>
-              <Col span={isMobile ? 24 : 16}>{device.network ? device.network.name : '无'}</Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              {' '}
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                电池电压(mV)
-              </Col>
-              <Col span={isMobile ? 24 : 16}>
-                {device.state && device.typeId !== DeviceType.Gateway
-                  ? device.state.batteryVoltage
-                  : '-'}
-              </Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                信号强度(dB)
-              </Col>
-              <Col span={isMobile ? 24 : 16}>{device.state ? device.state.signalLevel : '-'}</Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                固件版本号
-              </Col>
-              <Col span={isMobile ? 24 : 16}>
-                {device.information.firmware_version ? device.information.firmware_version : '-'}
-              </Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                最近连接时间
-              </Col>
-              <Col span={isMobile ? 24 : 16}>
-                {device.state.connectedAt
-                  ? moment(device.state.connectedAt * 1000).format('YYYY-MM-DD HH:mm:ss')
-                  : '-'}
-              </Col>
-            </Row>
-          </Col>
-          <Col span={isMobile ? 12 : 9}>
-            <Row>
-              <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                硬件版本
-              </Col>
-              <Col span={isMobile ? 24 : 16}>
-                {device.information.product_id ? device.information.product_id : '-'}
-              </Col>
-            </Row>
-          </Col>
-          {device.typeId !== DeviceType.Gateway && device.typeId !== DeviceType.Router && (
-            <Col span={isMobile ? 12 : 9}>
-              <Row>
-                <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                  最近一次采集时间
-                </Col>
-                <Col span={isMobile ? 24 : 16}>
-                  {device.data && device.data.timestamp && device.data.timestamp > 0
-                    ? moment.unix(device.data.timestamp).local().format('YYYY-MM-DD HH:mm:ss')
-                    : '-'}
-                </Col>
-              </Row>
-            </Col>
-          )}
-          {device.typeId === DeviceType.Gateway && (
-            <Col span={isMobile ? 12 : 9}>
-              <Row>
-                <Col span={isMobile ? 24 : 8} className='ts-detail-label'>
-                  IP地址
-                </Col>
-                <Col span={isMobile ? 24 : 16}>
-                  {device.information.ip_address ? (
-                    <Space>
-                      {device.information.ip_address}{' '}
-                      <a
-                        href={`http://${device.information.ip_address}`}
-                        target={'_blank'}
-                        style={{ fontSize: '10pt' }}
-                      >
-                        访问管理界面
-                      </a>
-                    </Space>
-                  ) : (
-                    '-'
-                  )}
-                </Col>
-              </Row>
-            </Col>
-          )}
-        </Row>
+        <SingleDeviceDetail device={device} upgradeStatus={upgradeStatus}/>
       </Skeleton>
     </ShadowCard>
   );
