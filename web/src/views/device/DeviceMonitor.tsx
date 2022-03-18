ed5d4a9890e1831889244dee9e051e0944a13d8f
liuzewei <liuzewei@outlook.com>
2022-03-18 15:12:13 +0800

    feature:mobile adaptation 8

--- web/src/views/device/DeviceMonitor.tsx
+++ web/src/views/device/DeviceMonitor.tsx
@@ -25,6 +25,7 @@ import { Network } from '../../types/network';
 import { GetNetworksRequest } from '../../apis/network';
 import { number } from 'echarts';
 import moment from 'moment';
+import { isMobile } from '../../utils/deviceDetection';
 
 const { Option } = Select;
 
@@ -103,7 +104,7 @@ const DeviceMonitor = () => {
 
     const commonOptions = {
       ...DefaultPieOption,
-      title: { text: '设备状态', left: '32%', top: '8%' },
+      title: { text: '设备状态', left: isMobile ? '50%' : '32%', top: isMobile ? 'auto' : '8%' },
       tooltip: {
         trigger: 'item',
         formatter: '{b}: {c} ({d}%)'
@@ -111,14 +112,14 @@ const DeviceMonitor = () => {
       legend: {
         orient: 'vertical',
         left: 'left',
-        bottom: '15%',
+        bottom: isMobile ? 'auto' : '15%',
         selectedMode: false
       },
       series: [
         {
           ...DefaultPieOption.series[0],
           radius: '50%',
-          center: ['40%', '50%']
+          center: [isMobile ? '50%' : '40%', '50%']
         }
       ]
     };
@@ -126,7 +127,7 @@ const DeviceMonitor = () => {
     return (
       <ShadowCard>
         <Row>
-          <Col span={8}>
+          <Col span={isMobile ? 24 : 8}>
             <EChartsReact
               option={{
                 ...commonOptions,
@@ -151,7 +152,7 @@ const DeviceMonitor = () => {
             />
           </Col>
           {levels.length > 0 && (
-            <Col span={8}>
+            <Col span={isMobile ? 24 : 8}>
               <EChartsReact
                 option={{
                   ...commonOptions,
@@ -171,15 +172,19 @@ const DeviceMonitor = () => {
             </Col>
           )}
           {countAlarm.length > 0 && (
-            <Col span={8}>
+            <Col span={isMobile ? 24 : 8}>
               <EChartsReact
                 option={{
                   ...DefaultMultiBarOption,
-                  title: { text: '一周报警趋势', left: '40%', top: '8%' },
+                  title: {
+                    text: '一周报警趋势',
+                    left: isMobile ? '50%' : '40%',
+                    top: isMobile ? 'auto' : '8%'
+                  },
                   legend: {
                     orient: 'vertical',
                     left: 'left',
-                    bottom: '15%',
+                    bottom: isMobile ? 'auto' : '15%',
                     selectedMode: false
                   },
                   grid: { left: '28%', top: '30%' },
@@ -224,7 +229,7 @@ const DeviceMonitor = () => {
         <MyBreadcrumb />
         <ShadowCard>
           <Row>
-            <Col span={4}>
+            <Col span={isMobile ? 18 : 4}>
               <Label name={'网络'}>
                 <Select
                   bordered={false}
