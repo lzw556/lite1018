9d499a3d5ee00606f89760c7f2d0beb07248f16b
liuzewei <liuzewei@outlook.com>
2022-03-24 09:16:28 +0800

    feature:change the primary color of offline from yellow to gray

--- web/src/views/device/SingleDeviceStatus.tsx
+++ web/src/views/device/SingleDeviceStatus.tsx
@@ -1,6 +1,6 @@
 import { Tag } from 'antd';
 import * as React from 'react';
-import { ColorDanger, ColorHealth, ColorInfo, ColorWarn } from '../../constants/color';
+import { ColorDanger, ColorHealth, ColorInfo, ColorOffline, ColorWarn } from '../../constants/color';
 import { Device } from '../../types/device';
 
 export const SingleDeviceStatus: React.FC<Pick<Device, 'alertStates' | 'state'>> = ({
@@ -27,6 +27,6 @@ export const SingleDeviceStatus: React.FC<Pick<Device, 'alertStates' | 'state'>>
       return <Tag color={ColorHealth}>正常</Tag>;
     }
   } else {
-    return <Tag color={ColorWarn}>离线</Tag>;
+    return <Tag color={ColorOffline} style={{color:'#000'}}>离线</Tag>;
   }
 };
