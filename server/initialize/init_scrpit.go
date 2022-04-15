703874a7477a1d039c4b0e2fb7a7434fe14e1472
lin <onlylin312@qq.com>
2022-04-15 15:07:53 +0800

    merge init script

--- server/initialize/init_scrpit.go
+++ server/initialize/init_scrpit.go
@@ -5,6 +5,62 @@ import (
 	"gorm.io/gorm"
 )
 
+func initUsers(db *gorm.DB) error {
+	user := entity.User{
+		Username: "admin",
+		Password: "123456",
+	}
+	err := db.FirstOrCreate(&user, map[string]interface{}{"id": 1, "username": user.Username}).Error
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func initProject(db *gorm.DB) error {
+	project := entity.Project{
+		Name:        "默认项目",
+		Description: "默认项目",
+	}
+	err := db.FirstOrCreate(&project, map[string]interface{}{"name": "默认项目"}).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func initRoles(db *gorm.DB) error {
+	roles := []entity.Role{
+		{
+			ID:          1,
+			Name:        "平台管理员",
+			Description: "平台管理员角色, 拥有平台的所有权限",
+		},
+		{
+			ID:          2,
+			Name:        "项目管理员",
+			Description: "项目管理员角色, 拥有项目的所有权限, 但不能创建项目",
+		},
+		{
+			ID:          3,
+			Name:        "操作员",
+			Description: "操作员角色, 拥有项目的部分权限",
+		},
+		{
+			ID:          4,
+			Name:        "访客",
+			Description: "访客角色, 只能访问项目",
+		},
+	}
+	for _, role := range roles {
+		err := db.FirstOrCreate(&role, map[string]interface{}{"name": role.Name}).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func initMenus(db *gorm.DB) error {
 	menus := []entity.Menu{
 		{
