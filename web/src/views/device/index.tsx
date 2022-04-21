c224e7e9b184ece343d75a8a8b61fbeb93a7f74d
liuzewei <liuzewei@outlook.com>
2022-04-21 15:50:53 +0800

    feature:add the action of alarmrules editing to device's list

--- web/src/views/device/index.tsx
+++ web/src/views/device/index.tsx
@@ -5,6 +5,7 @@ import {
   Input,
   Menu,
   message,
+  Modal,
   Popconfirm,
   Row,
   Select,
@@ -55,6 +56,7 @@ import { isMobile } from '../../utils/deviceDetection';
 import { Link, useLocation } from 'react-router-dom';
 import { PagedOption } from '../../types/props';
 import EditCalibrateParas from './edit/editCalibrateParas';
+import { AlarmRuleSettings } from './detail/setting/alarmRuleSettings';
 
 const { Search } = Input;
 const { Option } = Select;
@@ -76,6 +78,7 @@ const DevicePage = () => {
   const { hasPermission, hasPermissions } = usePermission();
   const [refreshKey, setRefreshKey] = useState<number>(0);
   const [visibleCalibrate, setVisibleCalibrate] = useState(false);
+  const [visibleAlarmRules, setVisibleAlarmRules] = useState(false);
 
   useEffect(() => {
     const { index, size } = pagedOptions;
@@ -173,6 +176,7 @@ const DevicePage = () => {
       setDevice(data);
       setEditBaseInfoVisible(key === '1');
       setEditSettingVisible(key === '2');
+      setVisibleAlarmRules(key === '3');
     });
   };
 
@@ -189,6 +193,7 @@ const DevicePage = () => {
         {hasPermission(Permission.DeviceSettingsEdit) && record.typeId !== DeviceType.Router && (
           <Menu.Item key={2}>更新设备配置</Menu.Item>
         )}
+        {record.typeId !== DeviceType.Gateway && record.typeId !== DeviceType.Router && <Menu.Item key={3}>编辑报警规则</Menu.Item>}
       </Menu>
     );
   };
@@ -484,6 +489,19 @@ const DevicePage = () => {
             });
           }}
         />
+      )}
+     {visibleAlarmRules && device && (
+        <Modal
+          title='报警规则'
+          visible={visibleAlarmRules}
+          onCancel={() => {
+            setVisibleAlarmRules(false);
+            setDevice(undefined);
+          }}
+          footer={null}
+        >
+          <AlarmRuleSettings device={device} />
+        </Modal>
       )}
     </Content>
   );
