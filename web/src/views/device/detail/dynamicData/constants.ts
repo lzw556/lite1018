2a54301a5070b22250d38712457a0e3543a7e450
liuzewei <liuzewei@outlook.com>
2022-05-31 16:57:03 +0800

    rename '预警力' to '预紧力'

--- web/src/views/device/detail/dynamicData/constants.ts
+++ web/src/views/device/detail/dynamicData/constants.ts
@@ -2,13 +2,13 @@ export const DYNAMIC_DATA_BOLTELONGATION = {
   data_type: 1073872901,
   fields: [
     { label: '长度', value: 'dynamic_length', unit: 'mm' },
-    { label: '预警力', value: 'dynamic_preload', unit: 'kN' },
+    { label: '预紧力', value: 'dynamic_preload', unit: 'kN' },
     { label: '压强', value: 'dynamic_pressure', unit: 'MPa' },
     { label: '飞行时间', value: 'dynamic_tof', unit: 'ns' },
     { label: '加速度', value: 'dynamic_acceleration', unit: 'g' }
   ] as const,
   metaData: [
-    { label: '预警力', value: 'min_preload', unit: 'kN' },
+    { label: '预紧力', value: 'min_preload', unit: 'kN' },
     { label: '温度', value: 'temperature', unit: '℃' },
     { label: '长度', value: 'min_length', unit: 'mm' },
     { label: '缺陷位置', value: 'defect_location', unit: 'mm' },
