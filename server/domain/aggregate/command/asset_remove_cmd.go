0adaaad9428c1816c9ec68be541a9a10861bc089
Hudson2017 <Hudson1989@163.com>
2022-06-02 15:59:26 +0800

    Update asset_remove_cmd.go

--- server/domain/aggregate/command/asset_remove_cmd.go
+++ server/domain/aggregate/command/asset_remove_cmd.go
@@ -4,23 +4,63 @@ import (
 	"context"
 
 	"github.com/thetasensors/theta-cloud-lite/server/adapter/repository"
+	"github.com/thetasensors/theta-cloud-lite/server/domain/aggregate/query"
 	"github.com/thetasensors/theta-cloud-lite/server/domain/dependency"
 	"github.com/thetasensors/theta-cloud-lite/server/domain/entity"
+	spec "github.com/thetasensors/theta-cloud-lite/server/domain/specification"
+	"github.com/thetasensors/theta-cloud-lite/server/domain/vo"
+	"github.com/thetasensors/theta-cloud-lite/server/pkg/transaction"
 )
 
 type AssetRemoveCmd struct {
 	entity.Asset
 
-	assetRepo dependency.AssetRepository
+	assetRepo                        dependency.AssetRepository
+	monitoringPointRepo              dependency.MonitoringPointRepository
+	monitoringPointDeviceBindingRepo dependency.MonitoringPointDeviceBindingRepository
 }
 
 func NewAssetRemoveCmd() AssetRemoveCmd {
 	return AssetRemoveCmd{
-		assetRepo: repository.Asset{},
+		assetRepo:                        repository.Asset{},
+		monitoringPointRepo:              repository.MonitoringPoint{},
+		monitoringPointDeviceBindingRepo: repository.MonitoringPointDeviceBinding{},
 	}
 }
 
 func (cmd AssetRemoveCmd) Run() error {
-	ctx := context.TODO()
-	return cmd.assetRepo.Delete(ctx, cmd.Asset.ID)
+	q := query.NewAssetQuery()
+
+	asset, err := q.Get(cmd.Asset.ID)
+	if err != nil {
+		return err
+	}
+
+	return transaction.Execute(context.TODO(), func(txCtx context.Context) error {
+		return cmd.iterRemove(txCtx, &asset)
+	})
+}
+
+func (cmd AssetRemoveCmd) iterRemove(txCtx context.Context, asset *vo.Asset) error {
+	for _, mp := range asset.MonitoringPoints {
+		if err := cmd.monitoringPointDeviceBindingRepo.DeleteBySpecs(txCtx, spec.MonitoringPointIDEqSpec(mp.ID)); err != nil {
+			return err
+		}
+
+		if err := cmd.monitoringPointRepo.Delete(txCtx, mp.ID); err != nil {
+			return err
+		}
+	}
+
+	for _, child := range asset.Children {
+		if err := cmd.iterRemove(txCtx, child); err != nil {
+			return err
+		}
+	}
+
+	if err := cmd.assetRepo.Delete(txCtx, asset.ID); err != nil {
+		return err
+	}
+
+	return nil
 }
