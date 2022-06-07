a53aec72d312ba068e110dd1b62886732998691c
Hudson2017 <Hudson1989@163.com>
2022-06-07 16:06:59 +0800

    Update statistics

--- server/domain/vo/all_statistics.go
+++ server/domain/vo/all_statistics.go
@@ -1,10 +1,10 @@
 package vo
 
 type AllStatistics struct {
-	DeviceNum               uint `json:"deviceNum"`
-	DeviceAlarmNum          uint `json:"deviceAlarmNum"`
-	RootAssetNum            uint `json:"rootAssetNum"`
-	RootAssetAlarmNum       uint `json:"rootAssetAlarmNum"`
-	MonitoringPointNum      uint `json:"monitoringPointNum"`
-	MonitoringPointAlarmNum uint `json:"monitoringPointAlarmNum"`
+	DeviceNum               uint   `json:"deviceNum"`
+	DeviceOfflineNum        uint   `json:"deviceOfflineNum"`
+	RootAssetNum            uint   `json:"rootAssetNum"`
+	RootAssetAlarmNum       []uint `json:"rootAssetAlarmNum"`
+	MonitoringPointNum      uint   `json:"monitoringPointNum"`
+	MonitoringPointAlarmNum []uint `json:"monitoringPointAlarmNum"`
 }
