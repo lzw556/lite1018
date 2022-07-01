d05c0253342198f0b226c9ff43e4f2b13cd0fd23
Lin <747325123@qq.com>
2022-07-01 10:49:05 +0800

    bugfix: fix svt220 svt520 data decode bug

--- server/adapter/iot/sensor/kx122_decoder.go
+++ server/adapter/iot/sensor/kx122_decoder.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/mitchellh/mapstructure"
 	"github.com/thetasensors/theta-cloud-lite/server/domain/entity"
+	"math"
 )
 
 type Kx122Decoder struct{}
@@ -22,6 +23,7 @@ func (s Kx122Decoder) Decode(data []byte) (map[string]interface{}, error) {
 		odr        uint32
 		number     uint32
 		dataLength uint32
+		offset     float32
 	}, 3)
 	metadataBytes := data[:72]
 	offset := 0
@@ -32,8 +34,11 @@ func (s Kx122Decoder) Decode(data []byte) (map[string]interface{}, error) {
 		metadata[i].odr = binary.LittleEndian.Uint32(metadataBytes[offset+4 : offset+8])
 		metadata[i].number = binary.LittleEndian.Uint32(metadataBytes[offset+8 : offset+12])
 		metadata[i].dataLength = binary.LittleEndian.Uint32(metadataBytes[offset+12 : offset+16])
+		metadata[i].offset = math.Float32frombits(binary.LittleEndian.Uint32(metadataBytes[offset+20 : offset+24]))
 		offset += 24
 	}
+
+	fmt.Println(fmt.Sprintf("%+v", metadata))
 
 	// decode raw data
 	totalLength := int(metadata[0].dataLength + metadata[1].dataLength + metadata[2].dataLength)
@@ -46,9 +51,9 @@ func (s Kx122Decoder) Decode(data []byte) (map[string]interface{}, error) {
 			svtRawData.SetMetadata(int(m.axis), m.ranges, m.odr, m.number)
 			switch m.dataType {
 			case 3: // int16
-				svtRawData.SetValues(int(m.axis), decodeInt16(axisData))
+				svtRawData.SetValues(int(m.axis), decodeInt16(axisData, m.offset))
 			case 6: // uint24
-				svtRawData.SetValues(int(m.axis), decodeUint24(axisData))
+				svtRawData.SetValues(int(m.axis), decodeUint24(axisData, m.offset))
 			}
 			rawDataOffset += int(m.dataLength)
 		}
@@ -61,26 +66,26 @@ func (s Kx122Decoder) Decode(data []byte) (map[string]interface{}, error) {
 	return nil, fmt.Errorf("invalid data length data length = %d, receive length = %d", totalLength, len(valueBytes))
 }
 
-func decodeInt16(data []byte) []float64 {
+func decodeInt16(data []byte, offset float32) []float64 {
 	j := 0
 	values := make([]float64, 0)
 	for j < len(data) {
 		var value = int16(binary.LittleEndian.Uint16(data[j : j+2]))
-		values = append(values, float64(value))
+		values = append(values, float64(value)-float64(offset))
 		j += 2
 	}
 	return values
 }
 
-func decodeUint24(data []byte) []float64 {
-	j := 0
+func decodeUint24(data []byte, offset float32) []float64 {
 	values := make([]float64, 0)
-	for j < len(data) {
-		fmt.Println(append(data[j:j+3], 0))
-		value := int32(binary.LittleEndian.Uint32(append(data[j:j+3], 0)))
-		fmt.Println(value)
-		values = append(values, float64(value-131072))
-		j += 3
+	for i := 0; i < len(data); i++ {
+		if i%3 == 0 {
+			v := make([]byte, 3)
+			copy(v, data[i:i+3])
+			value := int32(binary.LittleEndian.Uint32(append(v, 0)))
+			values = append(values, float64(value)-float64(offset))
+		}
 	}
 	return values
 }
