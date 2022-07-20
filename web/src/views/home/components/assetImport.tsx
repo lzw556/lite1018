73e1b1ccad0c1ffc7984c595c939ba139d231716
liuzewei <liuzewei@outlook.com>
2022-07-20 10:07:19 +0800

    issue: 117 121

--- web/src/views/home/components/assetImport.tsx
+++ web/src/views/home/components/assetImport.tsx
@@ -6,10 +6,12 @@ import { importAssets } from '../assetList/services';
 
 export const AssetImport: React.FC<{ onSuccess: () => void }> = ({ onSuccess }) => {
   const [loading, setLoading] = React.useState(false);
+  const fileInput = React.useRef<HTMLInputElement | null>(null);
   return (
     <div className='file-input' style={{ position: 'relative' }}>
       <input
         type='file'
+        ref={fileInput}
         style={{
           opacity: 0,
           position: 'absolute',
@@ -24,34 +26,31 @@ export const AssetImport: React.FC<{ onSuccess: () => void }> = ({ onSuccess }) 
           const files = e.target.files;
           if (files && files.length > 0) {
             const file = files[0];
-            try {
-              const reader = new FileReader();
-              reader.readAsText(file);
-              reader.onprogress = () => {
-                setLoading(true);
-              };
-              reader.onload = () => {
+            const reader = new FileReader();
+            reader.readAsText(file);
+            reader.onprogress = () => {
+              setLoading(true);
+            };
+            reader.onload = () => {
+              try {
                 const data = JSON.parse(reader.result as string);
-                importAssets(getProject(), data).then((res) => {
-                  setLoading(false);
-                  return new Promise<any>((resolve, reject) => {
+                importAssets(getProject(), data)
+                  .then((res) => {
                     if (res.data.code === 200) {
                       message.success('导入成功');
                       onSuccess();
-                      resolve(1);
                     } else {
                       message.error(`导入失败: ${res.data.msg}`);
-                      reject(res.data.msg);
                     }
-                  });
-                });
-              };
-            } catch (error) {
-              message.error('文件内部格式不正确');
-            } finally {
-              setLoading(false);
-              return false;
-            }
+                  })
+                  .finally(() => setLoading(false));
+              } catch (error) {
+                setLoading(false);
+                message.error('文件内部格式不正确');
+              } finally {
+                if (fileInput && fileInput.current) fileInput.current.value = '';
+              }
+            };
           }
         }}
       />
