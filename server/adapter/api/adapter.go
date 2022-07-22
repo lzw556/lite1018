752f6f58fbf0b5d3879d4f33f708dc9c3976c548
Hudson2017 <Hudson1989@163.com>
2022-07-22 13:55:13 +0800

    Update adapter.go

--- server/adapter/api/adapter.go
+++ server/adapter/api/adapter.go
@@ -100,7 +100,7 @@ func (a *Adapter) errorWrapper(handler middleware.ErrorWrapperHandler) gin.Handl
 				return
 			}
 			switch data := data.(type) {
-			case *vo.NetworkExportFile, *vo.ExcelFile, *vo.ImageFile, *vo.CsvFile, *vo.ProjectExported:
+			case *vo.NetworkExportFile, *vo.ExcelFile, *vo.ImageFile, *vo.CsvFile, *vo.ProjectExported, *vo.AlarmRuleGroupsExported:
 				if writer, ok := data.(response.FileWriter); ok {
 					response.WriteFile(ctx, writer)
 					return
