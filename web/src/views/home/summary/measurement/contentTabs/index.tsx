8ee03243cd3d031c42ff311909b98459a9eaac34
liuzewei <liuzewei@outlook.com>
2022-11-07 16:45:41 +0800

    remove unnecessary var

--- web/src/views/home/summary/measurement/contentTabs/index.tsx
+++ web/src/views/home/summary/measurement/contentTabs/index.tsx
@@ -13,35 +13,42 @@ export const MeasurementContents: React.FC<MeasurementRow & { onUpdate?: () => v
   measurement
 ) => {
   const dynamicDataConfigs = AppConfig.getDynamicDataConfigs(measurement.type);
-  const contents: Record<string, JSX.Element> = {
-    monitor: <Monitor {...measurement} />,
-    history: <HistoryData {...measurement} />,
-    dynamicData: <DynamicData {...measurement} />,
-    setting: <MeasurementSettings {...measurement} />,
-    alarmRecord: <FilterableAlarmRecordTable sourceId={measurement.id} />
-  };
-  const [key, setKey] = React.useState('monitor');
   const { hasPermission } = usePermission();
   const getTabList = () => {
-    const tabList: { key: string; tab: string }[] = [
-      { key: 'monitor', tab: '监控' },
-      { key: 'history', tab: '历史数据' }
+    const tabList: { key: string; tab: string; content: JSX.Element }[] = [
+      { key: 'monitor', tab: '监控', content: <Monitor {...measurement} /> },
+      { key: 'history', tab: '历史数据', content: <HistoryData {...measurement} /> }
     ];
-    if (dynamicDataConfigs) tabList.push({ key: 'dynamicData', tab: dynamicDataConfigs.title });
+    if (dynamicDataConfigs)
+      tabList.push({
+        key: 'dynamicData',
+        tab: dynamicDataConfigs.title,
+        content: <DynamicData {...measurement} />
+      });
     if (hasPermission(Permission.MeasurementEdit))
-      tabList.push({ key: 'setting', tab: '配置信息' });
-    tabList.push({ key: 'alarmRecord', tab: '报警记录' });
+      tabList.push({
+        key: 'setting',
+        tab: '配置信息',
+        content: <MeasurementSettings {...measurement} />
+      });
+    tabList.push({
+      key: 'alarmRecord',
+      tab: '报警记录',
+      content: <FilterableAlarmRecordTable sourceId={measurement.id} />
+    });
     return tabList;
   };
+  const tabList = getTabList();
+  const [key, setKey] = React.useState(tabList.length > 0 ? tabList[0].key : 'monitor');
 
   return (
     <ShadowCard
-      tabList={getTabList()}
+      tabList={tabList}
       onTabChange={(key) => {
         setKey(key);
       }}
     >
-      {key && contents[key]}
+      {key && tabList.find((tab) => tab.key === key)?.content}
     </ShadowCard>
   );
 };
