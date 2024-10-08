e2460dca4b17f6153f66832559d0902efaf5f80c
liuzewei <liuzewei@outlook.com>
2024-10-08 10:25:22 +0800

    improve tabsCard

--- src/components/tabsCard.tsx
+++ src/components/tabsCard.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Card, CardProps, Tabs, TabsProps } from 'antd';
 
-export const TabsCard = ({ cardProps, items, ...props }: TabsProps & { cardProps?: CardProps }) => {
+export const TabsCard = ({
+  cardProps,
+  items,
+  tabsRighted,
+  ...props
+}: TabsProps & { cardProps?: CardProps; tabsRighted?: boolean }) => {
+  const { className, ...rest } = props;
+  let newClassName = className;
+  if (tabsRighted) {
+    newClassName = newClassName ? `${newClassName} ant-tabs-righted` : `ant-tabs-righted`;
+  }
   return (
     <Card {...cardProps} bodyStyle={{ paddingTop: 0 }}>
-      <Tabs items={items} {...props} />
+      <Tabs items={items} {...{ ...rest, className: newClassName }} />
     </Card>
   );
 };
